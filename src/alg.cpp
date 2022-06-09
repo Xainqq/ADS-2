e952b638cf9a1de81eb40580890659b8897394e4
Водолагин Артём <52680620+Xainqq@users.noreply.github.com>
2022-06-09 23:00:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,49 +4,49 @@
 
 
 double pown(double value, uint16_t n) {
-double step = value;
-for (uint16_t i=1; i < n; i++) {
-step = step * value;
-}
-if (n == 0) {
-step = 1;
-}
-return(step);
+  double step = value;
+  for (uint16_t i=1; i < n; i++) {
+    step = step * value;
+  }
+  if (n == 0) {
+    step = 1;
+  }
+  return(step);
 }
 
 uint64_t fact(uint16_t n) {
-uint64_t s = 1;
-for (uint16_t i = 1; i <= n; i++) {
-s = s * i;
-}
+  uint64_t s = 1;
+  for (uint16_t i = 1; i <= n; i++) {
+    s = s * i;
+  }
 return(s);
 }
 
 double calcItem(double x, uint16_t n) {
-double s = pown(x, n) / fact(n);
-return(s);
+  double s = pown(x, n) / fact(n);
+  return(s);
 }
 
 double expn(double x, uint16_t count) {
-double s = 1;
-for (uint16_t i = 1; i <= count; i++) {
-s += calcItem(x, i);
-}
+  double s = 1;
+  for (uint16_t i = 1; i <= count; i++) {
+    s += calcItem(x, i);
+  }
 return(s);
 }
 
 double sinn(double x, uint16_t count) {
-double s = 0;
-for (uint16_t i = 1; i <= count; i++) {
-s += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
-}
+  double s = 0;
+  for (uint16_t i = 1; i <= count; i++) {
+    s += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+  }
 return(s);
 }
 
 double cosn(double x, uint16_t count) {
-double c = 0;
-for (uint16_t i = 1; i <= count; i++) {
-c += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
-}
+  double c = 0;
+  for (uint16_t i = 1; i <= count; i++) {
+    c += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+  }
 return(c);
 }
